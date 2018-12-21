4c2be09d6ebe98b69b97a93de734c672044f3cef
panxuesen <panxuesen520@gmail.com>
2018-12-21 12:33:03 +0800

    First Commit

---
+++ Project/STM32F10x_StdPeriph_Template/config.h
@@ -0,0 +1,7 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define DEBUG_NAME_DEFAULT "app"
+#define DEBUG_LEVEL_DEFAULT 4
+
+#endif
---
+++ Project/STM32F10x_StdPeriph_Template/debug.h
@@ -0,0 +1,76 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#include "config.h"
+
+#ifndef DEBUG_LEVEL
+#define DEBUG_LEVEL DEBUG_LEVEL_DEFAULT
+#endif
+
+#ifndef DEBUG_NAME
+#define DEBUG_NAME DEBUG_NAME_DEFAULT
+#endif
+
+#if DEBUG_LEVEL&&DEBUG_LEVEL_DEFAULT
+
+#include "SEGGER_RTT.h"
+
+#define log_init() SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL)
+
+    #if DEBUG_LEVEL >= 4&&DEBUG_LEVEL_DEFAULT>=4
+        #define log_error(...)                                                     \
+            SEGGER_RTT_printf(0,RTT_CTRL_TEXT_BRIGHT_RED"%s <error> ",DEBUG_NAME); \
+            SEGGER_RTT_printf(0,__VA_ARGS__)
+        #define log_warning(...)                                                        \
+            SEGGER_RTT_printf(0,RTT_CTRL_TEXT_BRIGHT_YELLOW"%s <warning> ",DEBUG_NAME); \
+            SEGGER_RTT_printf(0,__VA_ARGS__)
+        #define log_info(...)                                                           \
+            SEGGER_RTT_printf(0,RTT_CTRL_TEXT_BRIGHT_GREEN"%s <info> ",DEBUG_NAME);  \
+            SEGGER_RTT_printf(0,__VA_ARGS__)
+        #define log_debug(...)                                                        \
+            SEGGER_RTT_printf(0,RTT_CTRL_TEXT_BRIGHT_GREEN"%s <debug> ",DEBUG_NAME);  \
+            SEGGER_RTT_printf(0,__VA_ARGS__)
+
+    #elif DEBUG_LEVEL >= 3&&DEBUG_LEVEL_DEFAULT>=3
+        #define log_error(...)                                                     \
+            SEGGER_RTT_printf(0,RTT_CTRL_TEXT_BRIGHT_RED"%s <error> ",DEBUG_NAME); \
+            SEGGER_RTT_printf(0,__VA_ARGS__)
+        #define log_warning(...)                                                        \
+            SEGGER_RTT_printf(0,RTT_CTRL_TEXT_BRIGHT_YELLOW"%s <warning> ",DEBUG_NAME); \
+            SEGGER_RTT_printf(0,__VA_ARGS__)
+        #define log_info(...)                                                           \
+            SEGGER_RTT_printf(0,RTT_CTRL_TEXT_BRIGHT_GREEN"%s <info> ",DEBUG_NAME);  \
+            SEGGER_RTT_printf(0,__VA_ARGS__)
+        #define log_debug(...) 
+
+    #elif DEBUG_LEVEL >= 2&&DEBUG_LEVEL_DEFAULT>=2
+        #define log_error(...)                                                     \
+            SEGGER_RTT_printf(0,RTT_CTRL_TEXT_BRIGHT_RED"%s <error> ",DEBUG_NAME); \
+            SEGGER_RTT_printf(0,__VA_ARGS__)
+        #define log_warning(...)                                                        \
+            SEGGER_RTT_printf(0,RTT_CTRL_TEXT_BRIGHT_YELLOW"%s <warning> ",DEBUG_NAME); \
+            SEGGER_RTT_printf(0,__VA_ARGS__)
+        #define log_info(...) 
+        #define log_debug(...)  
+
+    #elif DEBUG_LEVEL >= 1&&DEBUG_LEVEL_DEFAULT>=1
+        #define log_error(...)                                                     \
+            SEGGER_RTT_printf(0,RTT_CTRL_TEXT_BRIGHT_RED"%s <error> ",DEBUG_NAME); \
+            SEGGER_RTT_printf(0,__VA_ARGS__)
+        #define log_warning(...) 
+        #define log_info(...) 
+        #define log_debug(...) 
+
+    #endif
+#else
+
+    #define log_init() 
+    #define log_error(...) 
+    #define log_warning(...) 
+    #define log_info(...) 
+    #define log_debug(...) 
+
+#endif
+
+#endif
+
